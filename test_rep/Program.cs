1fa3821469fdbc2a0420c29c0c5fb4f160c3fb6a
andreylar31415@gmail.com <andreylar31415@gmail.com>
2020-05-30 18:40:45 +0700

    добавлен :"World Hello"

--- test_rep/Program.cs
+++ test_rep/Program.cs
@@ -7,6 +7,7 @@ namespace test_rep
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("World Hello!");
         }
     }
 }
