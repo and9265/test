3bc5e72d2b859d11fff03f84740851fe80d15000
andreylar31415@gmail.com <andreylar31415@gmail.com>
2020-05-30 18:18:46 +0700

    Добавьте файлы проекта.

---
+++ test_rep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_rep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
